6d0d6cad41130663513b17db700c089b5469f687
Tin Pham <phamkientin@gmail.com>
2020-08-25 15:38:42 +0700

    Fix

--- src/api/news.js
+++ src/api/news.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
 
 const _url = 'http://newsapi.org/v2/everything?q=corona&from=2020-08-25&sortBy=publishedAt&apiKey=c84d54c780c74f9fbd3b3ddd6532976d';
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 export const fetchnewsdata = async () => {
     try {
-        const { data } = await axios.get(_url, {
-            mode: 'cors',
-            credentials: 'include'
-        });
+        const { data } = await axios.get(_url);
         return data.articles;
     } catch (error) {
         console.log(error);
