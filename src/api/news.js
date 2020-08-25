35e1509d4d3396734c2e13aea444bcde839b396a
Tin Pham <phamkientin@gmail.com>
2020-08-25 15:17:07 +0700

    fix css

--- src/api/news.js
+++ src/api/news.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
 
 const _url = 'http://newsapi.org/v2/everything?q=corona&from=2020-08-25&sortBy=publishedAt&apiKey=c84d54c780c74f9fbd3b3ddd6532976d';
-const _service = 'http://tyn-ars.herokuapp.com/';
+
 export const fetchnewsdata = async () => {
     try {
-        const { data } = await axios.get(_url, _service);
+        const { data } = await axios.get(_url, {
+            mode: 'cors',
+            credentials: 'include'
+        });
         return data.articles;
     } catch (error) {
         console.log(error);
