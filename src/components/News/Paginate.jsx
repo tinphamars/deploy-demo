391e334b960c437ad89edfe7b76c009b8448bb36
Tin Pham <phamkientin@gmail.com>
2020-08-28 09:32:37 +0700

    Fix disabled

--- src/components/News/Paginate.jsx
+++ src/components/News/Paginate.jsx
@@ -13,13 +13,13 @@ const Paginate = (props) => {
         <nav aria-label="Page navigation example">
             <ul className="pagination">
                 <li className={_page == 1 ? "disabled page-item" : "page-item"} >
-                    <a className="page-link" href="#" aria-label="Previous" onClick={() => onClickCallNewsPage(_page - 1)}>
+                    <a className="page-link" href="#" aria-label="Previous" onClick={_page > 1 ? () => onClickCallNewsPage(_page - 1) : 'javscrip;'}>
                         <span aria-hidden="true">«</span>
                         <span className="sr-only">Previous</span>
                     </a>
                 </li>
                 <li className={_page >= countPage ? "disabled page-item" : "page-item"}>
-                    <a className="page-link" href="#" aria-label="Next" onClick={() => onClickCallNewsPage(_page + 1)}>
+                    <a className="page-link" href="#" aria-label="Next" onClick={_page < countPage ? () => onClickCallNewsPage(_page + 1) : 'javscrip;'}>
                         <span aria-hidden="true">»</span>
                         <span className="sr-only">Next</span>
                     </a>
