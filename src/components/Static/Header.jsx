f9b2939292b052a4f74d8f4b1de4ab51dddc08d3
Tin Pham <phamkientin@gmail.com>
2021-03-04 11:28:34 +0700

    rename menu

--- src/components/Static/Header.jsx
+++ src/components/Static/Header.jsx
@@ -8,16 +8,16 @@ const Header = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
              <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                <Link className="navbar-brand" to="/">TYN</Link>
+                <Link className="navbar-brand" to="/">Chart</Link>
                 <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
                     <li className="nav-item">
-                        <Link className="nav-link" to="/blog">Blog</Link>
+                        <Link className="nav-link" to="/blog">Pagination</Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/product">Product</Link>
+                        <Link className="nav-link" to="/product">More load</Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/detail">Detail</Link>
+                        <Link className="nav-link" to="/detail">noe</Link>
                     </li>
                 </ul>
             </div>
